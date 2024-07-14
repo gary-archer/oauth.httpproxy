70160cdcd17ec34ab53686e5b4e886cfa657d309
gary-archer <garyarcher36@gmail.com>
2024-07-14 14:51:24 +0100

    Udpates

--- utils/activate.sh
+++ utils/activate.sh
@@ -21,7 +21,7 @@ if [ $? -ne 0 ]; then
 fi
 
 #
-# Also set the command line proxy
+# Also set the command line proxy if required
 #
-export http_proxy='http://127.0.0.1:8888'
-export https_proxy='http://127.0.0.1:8888'
+#export http_proxy='http://127.0.0.1:8888'
+#export https_proxy='http://127.0.0.1:8888'
